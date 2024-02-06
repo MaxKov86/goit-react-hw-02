f43e09b714deb38998caa982ba4b39ce48bfb1de
MaxKov86 <max.kov86@ukr.net>
2024-02-06 17:11:42 +0200

    Updated App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Description } from "./Description/Description";
 import { Options } from "./Options/Options";
 import { Feedback } from "./Feedback/Feedback";
 
 function App() {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const [feedback, setFeedback] = useState(() => {
+    const savedFeedbacks = window.localStorage.getItem("saved-feedbacks");
+    if (savedFeedbacks !== null) {
+      return JSON.parse(savedFeedbacks);
+    }
+    return {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+    };
   });
 
+  useEffect(() => {
+    window.localStorage.setItem("saved-feedbacks", JSON.stringify(feedback));
+  }, [feedback]);
+
   const totalFeedback = feedback.good + feedback.neutral + feedback.bad;
+
   const updateGoodFeedback = () => {
     setFeedback({
       ...feedback,
       good: feedback.good + 1,
     });
   };
+
   const updateNeutralFeedback = () => {
     setFeedback({
       ...feedback,
